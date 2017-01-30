525b883fcb17ea1831b93f905f0019e8fe4ee0ab
Trent Folkman <tfolk17@gmail.com>
2017-01-30 15:12:20 -0700

    Canny Approach

--- src/ImagePrep.py
+++ src/ImagePrep.py
@@ -6,21 +6,21 @@ import cv2
 def prep(img):
 
     # Reduce Image Size or create image pyramid
-    #img = cv2.resize(img, None, fx=1/10, fy=1/10, interpolation = cv2.INTER_AREA)
+    img = cv2.resize(img, None, fx=1/10, fy=1/10, interpolation = cv2.INTER_AREA)
 
     # Histogram Stretch Image
     histStretch = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
     img = histStretch.apply(img)
 
     # Median Filter (get rid of text, background noise).
-    img = cv2.medianBlur(img, 151)
+    img = cv2.medianBlur(img, 51)
 
     # Automatically threshold image into foreground, background
-    #img = cv2.Canny(img, 105, 120)
-    __,img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    img = cv2.Canny(img, 20, 25)
+    #__,img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
     # Resize image back to original
-    #img = cv2.resize(img, None, fx=10, fy=10, interpolation = cv2.INTER_CUBIC)
+    img = cv2.resize(img, None, fx=10, fy=10, interpolation = cv2.INTER_CUBIC)
 
     imgout = img
     return imgout
